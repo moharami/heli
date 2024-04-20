3c27f9d821fc5c8dfc0d7976781ddffaae01d8ad
Amir <a.moharami@gmail.com>
2024-04-20 11:26:04 +0330

    develop login and register

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\API\RegisterController;
+use App\Http\Controllers\API\ProductController;
+
+Route::controller(RegisterController::class)->group(function(){
+    Route::post('register', 'register');
+    Route::post('login', 'login');
+});
+        
